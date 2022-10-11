2e0e02b9d5b6e275eeced39e576b737c5273046a
dawnarc <aiguo1024@gmail.com>
2022-10-11 14:41:21 +0800

    add README.md for bind testing

---
+++ 01.bind/README.md
@@ -0,0 +1,5 @@
+# Lua binding example
+
+Origin:  
+Example of Lua in C++ and userdata objects  
+https://gist.github.com/Youka/2a6e69584672f7cb0331
--- 02.reload/build.bat
+++ 02.reload/build.bat
@@ -1,6 +1,11 @@
+::clean cache
 if exist "%~dp0\build\" rd /q /s "%~dp0\build"
 mkdir build & cd build
 
+::build
 cmake -G Ninja -DCMAKE_BUILD_TYPE=Release ../
 cmake --build .
 cd ..
+
+::run test case
+start "" "CMD " /c TestReload.exe ^& pause
