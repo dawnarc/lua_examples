6783700e34de86b385b2d2cd8b0aa25eaefe9973
dawnarc <aiguo1024@gmail.com>
2022-10-11 15:00:33 +0800

    format: replace tab with 4 spaces

--- 02.reload/main.cpp
+++ 02.reload/main.cpp
@@ -6,23 +6,23 @@
 
 
 int main(int argc, char **argv) {
-	int status, result;
-	lua_State *L;
-	L = luaL_newstate();
-	luaL_openlibs(L);
-	status = luaL_loadfile(L, "main.lua");
+    int status, result;
+    lua_State *L;
+    L = luaL_newstate();
+    luaL_openlibs(L);
+    status = luaL_loadfile(L, "main.lua");
 
-	if (status) {
-		fprintf(stderr, "Couldn't load file: %s\n", lua_tostring(L, -1));
-		exit(1);
-	}
-	result = lua_pcall(L, 0, LUA_MULTRET, 0);
+    if (status) {
+        fprintf(stderr, "Couldn't load file: %s\n", lua_tostring(L, -1));
+        exit(1);
+    }
+    result = lua_pcall(L, 0, LUA_MULTRET, 0);
 
-	if (result) {
-		fprintf(stderr, "Failed to run script: %s\n", lua_tostring(L, -1));
-		exit(1);
-	}
-	
-	lua_close(L);
-	return 0;
+    if (result) {
+        fprintf(stderr, "Failed to run script: %s\n", lua_tostring(L, -1));
+        exit(1);
+    }
+    
+    lua_close(L);
+    return 0;
 }
